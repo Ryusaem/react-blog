954e976c61c4f4e31d9fe679beda77446dcd95d3
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-07-03 19:55:29 +0200

    testing with home and blogList component

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title }) => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="blog-list">
       <h2>{title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
 import BlogList from "./BlogList";
-import useFetch from "./useFetch";
+import { useState } from "react";
+// import useFetch from "./useFetch";
 
 const Home = () => {
-
-  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs'); 
+  // const {
+  //   data: blogs,
+  //   isPending,
+  //   error,
+  // } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">

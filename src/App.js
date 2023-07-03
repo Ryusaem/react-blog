9c339dc6c7c8e7b599fe3c27215db3504b3411fb
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2023-07-03 16:59:26 +0200

    final version for json-server

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Create from "./Create";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BlogDetails from "./BlogDetail";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
